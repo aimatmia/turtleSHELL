5c5644402018263692141014f67cc2867df98060
Mia <mobrien@stuy.edu>
2016-12-02 08:03:58 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <limits.h>
+
+#include "t_rex.h"
+#include "shell.h"
+
+int main(){
+	while(1){
+		int fdin, fdout;
+		fdin = fdout = -1;
+		char buf[256];
+		prompt();
+		readin(buf);        
+		parse(buf);
+           printf("whats wrong\n");
+	}
+	return 0;
+}
