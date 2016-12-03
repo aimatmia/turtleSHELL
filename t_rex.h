8eb66f17d13a13b11356bc3430881c4693d5fc54
jieyuxu <jxu9@stuy.edu>
2016-12-02 22:19:27 -0500

    Update t_rex.h

--- t_rex.h
+++ t_rex.h
@@ -2,12 +2,13 @@
 #define T_REX
 
 int cd(char *pth);
-char * trim(char **str);
+// char * trim(char **str);
 void readin(char *buf);
 void decisonmaker(char * buf);
 void peterpiper(char * buf);
 void redirect(char * buf);
 void exec(char ** cmd, int fdin, int fdout);
 void redirectR(char * buf);
+void redirectRA(char * buf);
 void redirectL(char * buf);
 #endif
