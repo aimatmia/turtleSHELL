d10dc46ea6c724dc0db9c84cd3ff8cc80ad7b3e1
Mia <mobrien@stuy.edu>
2016-12-03 11:11:28 -0500

    Update t_rex.h

--- t_rex.h
+++ t_rex.h
@@ -4,12 +4,9 @@
 int cd(char *pth);
 // char * trim(char **str);
 void readin(char *buf);
-void decisonmaker(char * buf);
+void parse(char * buf);
 void peterpiper(char * buf);
+void redirect(char * bufadd); 
+void exec(char** cmd, int fin, int fout);
 void redirect(char * buf);
-void exec(char ** cmd, int fdin, int fdout);
-void redirectR(char * buf);
-void redirectRA(char * buf);
-void redirectL(char * buf);
-void exec_1com(char* buf, char *fin, char *fout);
 #endif
